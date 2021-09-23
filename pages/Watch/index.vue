27a0b510c60eac021370cd93eff2e008dfe7685a
Zain Ahmed <zain.ahmed199524@gmail.com>
2021-09-23 17:51:58 +0500

    udpate

--- pages/Watch/index.vue
+++ pages/Watch/index.vue
@@ -1,11 +1,26 @@
 <template>
-  <div>
-    <h2>watch</h2>
+  <div style="height: 100vh">
+    <div class="watch-screen-back-btn">
+      <font-awesome-icon
+        @click="handleBack"
+        class="text-light"
+        style="
+          width: 50px;
+          box-shadow: 1px 1px 6px 0px rgb(86 86 86);
+          height: 50px;
+          position: absolute;
+          background-color: rgb(0, 131, 208);
+          border-radius: 50px;
+          padding: 5px;
+          margin: 5px;
+        "
+        :icon="['fas', 'chevron-left']"
+      />
+    </div>
     <div class="watch-video-container">
-      <div class="video-player-container" style="height: 400px">
+      <div class="video-player-container" style="height: 100vh">
         <YoutubePlayer :videoUrl="videoUrl" />
       </div>
-      <div class="back-btn"></div>
     </div>
   </div>
 </template>
