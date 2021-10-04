cde1a2e275b09735cc509f7c2bcb4e39ae995c45
Zain Ahmed <zain.ahmed@backcountry.com>
2021-10-04 20:14:59 +0500

    update

--- configurations/keys.js
+++ configurations/keys.js
@@ -3,19 +3,19 @@ import constants from './constants'
 const keys = {
   baseURL: constants.localBaseUrl,
 }
+
 keys.NUXT_ENV_PAYPAL_KEY = process.env.NUXT_ENV_PAYPAL_KEY
-keys.NUXT_ENV_PAYPAL_KEY = process.env.NUXT_ENV_PAYPAL_KEY
-console.log('ENV_ENVIRONMENT::', process.env.ENV_ENVIRONMENT)
-console.log('NUXT_ENV_KEY::', process.env.NUXT_ENV_KEY)
-if (process.env.ENV_ENVIRONMENT === 'localhost') {
+keys.NUXT_ENV_STRIPE_KEY = process.env.NUXT_ENV_STRIPE_KEY
+
+if (process.env.NUXT_ENV_enviroment === 'localhost') {
   keys.baseURL = constants.localBaseUrl
 }
 
-if (process.env.ENV_ENVIRONMENT === 'development') {
+if (process.env.NUXT_ENV_enviroment === 'development') {
   keys.baseURL = constants.developmentBaseUrl
 }
 
-if (process.env.ENV_ENVIRONMENT === 'production') {
+if (process.env.NUXT_ENV_enviroment === 'production') {
   keys.baseURL = constants.productionBaseUrl
 }
 
