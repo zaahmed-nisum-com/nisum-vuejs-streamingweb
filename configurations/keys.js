3eefd4d3c26e4bb5a0c9693e5b9e4e9ae6e41e04
Zain Ahmed <zain.ahmed@backcountry.com>
2021-10-04 18:06:37 +0500

    working on config env varibales

--- configurations/keys.js
+++ configurations/keys.js
@@ -6,21 +6,21 @@ const keys = {
 keys.PAYPAL_SANDBOX_KEY = process.env.PAYPAL_SANDBOX_KEY
 console.log('ENVRIONEMENT::: ', process.env)
 console.log('NUXT_ENV_MY_VARIABLE::', process.env.NUXT_ENV_MY_VARIABLE)
-console.log(process.env.enviroment)
+console.log('ENV_ENVIRONMENT::', process.env.ENV_ENVIRONMENT)
 console.log(process.env.TEST_KEY)
 console.log(process.env.HOST)
 console.log(process.env.STRIPE_PUBLIC_DUMMY_KEY)
 console.log(process.env.PAYPAL_SANDBOX_KEY)
 console.log('key')
-if (process.env.enviroment === 'localhost') {
+if (process.env.ENV_ENVIRONMENT === 'localhost') {
   keys.baseURL = constants.localBaseUrl
 }
 
-if (process.env.enviroment === 'development') {
+if (process.env.ENV_ENVIRONMENT === 'development') {
   keys.baseURL = constants.developmentBaseUrl
 }
 
-if (process.env.enviroment === 'production') {
+if (process.env.ENV_ENVIRONMENT === 'production') {
   keys.baseURL = constants.productionBaseUrl
 }
 
